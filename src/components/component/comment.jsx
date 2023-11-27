52b02bbb56752aaef35623b34cf36be43a988ee6
Kristian <krisplakcoder@gmail.com>
2023-11-26 22:16:37 -0800

    completed styling on header, video, and comments

--- src/components/component/comment.jsx
+++ src/components/component/comment.jsx
@@ -16,8 +16,8 @@ export default function Comment( {commentArray}) {
                 </div>
                 <div className="comment__container">
                     <form className="comment__form">
-                        <label className="comment__form-label" for = "comment-box">Join The Conversation</label>
-                        <textarea className="comment__form-box" typeof="text" id="comment-box" name="comment-box">Add a new comment</textarea>
+                        <div className="comment__form--position"><label className="comment__form-label" for = "comment-box">Join The Conversation</label>
+                        <textarea className="comment__form-box" typeof="text" id="comment-box" name="comment-box">Add a new comment</textarea></div>
                         <button className="comment__form-button" type="submit" value="submit">Comment</button>
                     </form>
                 </div>
--- src/components/component/stats.jsx
+++ src/components/component/stats.jsx
@@ -5,7 +5,7 @@ export default function VideoStats({author, date, viewCount, likes}) {
 
     return (
         <><section className="vid-stats__container"><div className="vid-stats">
-            <p className="vid-stats--bold">{author}</p>
+            <p className="vid-stats--bold">By {author}</p>
             <p>{new Date(date).toLocaleDateString()}</p>
         </div><div className="vid-stats">
                 <p><img className="vid-stats__icon" src={ViewsIcon} alt="views" />{viewCount}</p>
